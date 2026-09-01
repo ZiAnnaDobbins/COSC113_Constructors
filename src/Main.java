608736ba2fb219929785c69a49ba529cf7fa5897
ZiAnnaDobbins <ziannadobbins@gmail.com>
2026-09-01 09:41:03 -0400

    constructors

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter
+public class Student {
+    //Attribuites
+    String FN;
+    String LN;
+    int Sid;
+
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+    public static void main(String[] argument) {
+
+        Student s1 = new Student();
+        System.out.println("s1");
+
+
+
+
+        //use of dot operator (.)
+        System.out.println("Frist name is: " + s1.FN);
+        System.out.println("Frist last is: " + s1.LN);
+        System.out.println("hello constructtors");
+
+    }
+}
